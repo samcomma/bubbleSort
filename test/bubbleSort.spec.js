2d1e63771998a7c32f23ade0f9fce3bbb30e5192
samcomma <samcomerford1825@gmail.com>
2018-07-10 23:22:10 -0400

    removed misplaced spec file

--- test/bubbleSort.spec.js
+++ test/bubbleSort.spec.js
@@ -1,31 +1,36 @@
-const chai = require('chai')
-const expect = chai.expect
-const bubbleSort = require('../bubblesort')
+const expect = require('chai').expect
+const sinon = require('sinon')
+const bubbleSort = require('../bubbleSort')
 
 
-describe('bubbleSort', function(){
+describe('bubbleSort', function() {
   it ('exists', function() {
-  	expect(bubbleSort).to.be.ok;
+    expect(bubbleSort).to.be.ok;
   });
 
   it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+    expect( bubbleSort([]) ).to.eql( [] );
   });
 
   it('handles an array with one element', function(){
-    expect( bubbleSort([2]) ).toEqual( [2] );
+    expect( bubbleSort([2]) ).to.eql( [2] );
   });
 
     it('handles an array with two elements', function(){
-    expect( bubbleSort([2, 1]) ).toEqual( [1, 2] );
+    expect( bubbleSort([2, 1]) ).to.eql( [1, 2] );
   });
 
   it('handles an array with multiple elements', function(){
-    expect( bubbleSort([2, 9, 4, 5]) ).toEqual( [2, 4, 5, 9] );
+    expect( bubbleSort([2, 9, 4, 5]) ).to.eql( [2, 4, 5, 9] );
   });
 
   it('works with zero', function(){
-  	expect( bubbleSort([1, 4, 0]) ).toEqual( [0, 1, 4])
-  })
-});
+    expect( bubbleSort([1, 4, 0]) ).to.eql( [0, 1, 4]);
+  });
 
+  it('is efficient', function(){
+    const spy = sinon.spy();
+    bubbleSort([1, 2, 3, 4, 5], spy);
+    expect(spy.callCount).to.equal(4);
+  });
+});
